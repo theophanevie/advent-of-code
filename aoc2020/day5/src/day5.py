52f613daa5018f815e9a1d0528a565815507930f
Théophane Vié <theophanevie@gmail.com>
2020-12-05 13:08:15 +0100

    day5: use string a bytes

--- aoc2020/day5/src/day5.py
+++ aoc2020/day5/src/day5.py
@@ -1,38 +1,17 @@
 import sys
 import math
 
-def getcolumn(bpass: str) -> int:
-    lpos = 0
-    rpos = 7
-    for i in range(7, 9, 1):
-        if bpass[i] == 'L':
-            rpos -= round((rpos - lpos) / 2)
-        else:
-            lpos += round((rpos - lpos) / 2)
-
-    if bpass[9] == 'L':
-        return lpos
-    return rpos
-
-
-def getrow(bpass: str) -> int:
-    lpos = 0
-    upos = 127
-    for i in range(6):
-        if bpass[i] == 'F':
-            upos -= round((upos - lpos) / 2)
-        else:
-            lpos += round((upos - lpos) / 2)
-
-    if bpass[6] == 'F':
-        return lpos
-    return upos
+def parsebpass(bpass: str, start: int, end: int) -> int:
+    res = 0
+    for i in range(start, end + 1):
+        res += 2 ** (end - i) if bpass[i] in 'BR' else 0
+    return res 
 
 def check_firstpart(inputfile: str) -> int:
     with open(inputfile,'r') as inputfile:
         maxid = 0
         for line in inputfile:
-            maxid = max(maxid, getrow(line) * 8 + getcolumn(line))
+            maxid = max(maxid, parsebpass(line, 0, 6) * 8 + parsebpass(line, 7, 9))
     return maxid
         
 
@@ -40,7 +19,7 @@ def check(inputfile: str) -> int:
     with open(inputfile,'r') as inputfile:
         l = []
         for line in inputfile:
-            l.append(getrow(line) * 8 + getcolumn(line))
+            l.append(parsebpass(line, 0, 6) * 8 + parsebpass(line, 7, 9))
         l.sort()
         for i in range(len(l) - 1):
             if l[i] + 1 != l[i + 1]:
@@ -49,4 +28,4 @@ def check(inputfile: str) -> int:
         
 
 if __name__ == "__main__":
-    print(check(sys.argv[1]))
+    print(check_firstpart(sys.argv[1]))
