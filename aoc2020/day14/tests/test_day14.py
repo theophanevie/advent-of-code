97e2f41fdc2b722f5fcb7f58ae5fafa74533bf28
Théophane Vié <theophanevie@gmail.com>
2020-12-15 01:28:18 +0100

    day14: add ugly test to hardcoded solution

--- aoc2020/day14/tests/test_day14.py
+++ aoc2020/day14/tests/test_day14.py
@@ -5,12 +5,15 @@ from ..src.day14 import check_secondpart, check_firstpart
 def test_basic():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     path = os.path.join('..', 'input', 'provided-test-simple.txt')
-    assert 1068781 == check_secondpart(path)
-    assert 295 == check_firstpart(path)
+    assert 165 == check_firstpart(path)
 
 def test_input():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     path = os.path.join('..', 'input', 'input.txt')
-    assert 530015546283687 == check_secondpart(path)
-    assert 207 == check_firstpart(path)
+    assert 13727901897109 == check_firstpart(path)
+
+def test_basic2():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    path = os.path.join('..', 'input', 'provided-test-part2.txt')
+    assert 208 == check_secondpart(path)
     
