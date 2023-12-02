709098af03a393de743d3c37e8b497cd252e1afc
Théophane Vié <theophane.vie@petit-nuage.org>
2023-12-02 16:31:38 +0100

    feat(2023): add day01

---
+++ aoc2023/day01/ex1/solver.py
@@ -0,0 +1,21 @@
+import sys
+import string
+
+
+def reader(input_file: str) -> str:
+    input_file = open(input_file, "r").readlines()
+    for line in input_file:
+        yield line.strip(string.ascii_lowercase + "\n")
+
+
+def main(input_file: str) -> int:
+    """
+    https://adventofcode.com/2023/day/1#part1
+    """
+    return sum([int(line[0] + line[-1]) for line in reader(input_file)])
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise ValueError(f"Invalid parameter, usage : {sys.argv[0]} <input_file.txt>")
+    print(main(sys.argv[1]))
---
+++ aoc2023/day01/ex2/solver.py
@@ -0,0 +1,35 @@
+import sys
+import string
+
+# Since eight-two is meant to be understood as 82, we can't replace all the letters with the number.
+# The maximum number of letters that can be reused from one digit to another is 1, so we need to keep the first and
+# last letters.
+DIGIT_AS_LETTERS = [
+    ("one", "o1e"),
+    ("two", "t2o"),
+    ("three", "t3e"),
+    ("four", "f4r"),
+    ("five", "f5e"),
+    ("six", "s6x"),
+    ("seven", "s7n"),
+    ("eight", "e8t"),
+    ("nine", "n9e")
+]
+
+
+def reader(input_file: str) -> str:
+    input_file = open(input_file, "r").readlines()
+    for line in input_file:
+        for search, replace in DIGIT_AS_LETTERS:
+            line = line.replace(search, replace)
+        yield line.strip(string.ascii_lowercase + "\n")
+
+
+def main(input_file: str) -> int:
+    return sum([int(line[0] + line[-1]) for line in reader(input_file)])
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise ValueError(f"Invalid parameter, usage : {sys.argv[0]} <input_file.txt>")
+    print(main(sys.argv[1]))
