5413a08a4a9e7c136e75506e372e80c1d15ff075
Théophane Vié <theophane.vie@petit-nuage.org>
2023-12-02 19:59:40 +0100

    fix(2023): coding style

--- aoc2023/day01/test_day01.py
+++ aoc2023/day01/test_day01.py
@@ -1,10 +1,12 @@
 from day01.ex1.solver import main as main_ex1
 from day01.ex2.solver import main as main_ex2
 
+DAY = "day01"
+
 
 def test_ex1():
-    assert main_ex1(f"day01/ex1/test.txt") == 142
+    assert main_ex1(f"{DAY}/ex1/test.txt") == 142
 
 
 def test_ex2():
-    assert main_ex2(f"day01/ex2/test.txt") == 281
+    assert main_ex2(f"{DAY}/ex2/test.txt") == 281
--- aoc2023/day02/test_day02.py
+++ aoc2023/day02/test_day02.py
@@ -1,10 +1,12 @@
 from day02.ex1.solver import main as main_ex1
 from day02.ex2.solver import main as main_ex2
 
+DAY = "day02"
+
 
 def test_ex1():
-    assert main_ex1(f"day02/ex1/test.txt") == 8
+    assert main_ex1(f"{DAY}/ex1/test.txt") == 8
 
 
 def test_ex2():
-    assert main_ex2(f"day02/ex2/test.txt") == 2286
+    assert main_ex2(f"{DAY}/ex2/test.txt") == 2286
