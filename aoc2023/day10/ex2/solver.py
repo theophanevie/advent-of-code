735ce73be1d484ca2d6d1af4ca086a75cc718e8d
Théophane Vié <theophane.vie@petit-nuage.org>
2023-12-10 17:45:38 +0100

    feat(2023-d10-ex2): init

---
+++ aoc2023/day10/ex2/solver.py
@@ -0,0 +1,118 @@
+import sys
+
+POS = tuple[int, int]
+
+
+def parse_input(input_file: str) -> tuple[dict[POS, str], POS]:
+    input_file = open(input_file, "r")
+    grid = {}
+
+    for y, line in enumerate(input_file.readlines()):
+        line = line.strip()
+        for x, pipe in enumerate(line):
+            grid[x, y] = pipe
+
+            if pipe == "S":
+                start_pos = (x, y)
+
+    return grid, start_pos
+
+
+def find_next(grid: dict[POS, str], cur_pos: POS, from_pos: POS) -> POS | None:
+    deltas = []
+
+    match grid[cur_pos]:
+
+        case "|":
+            deltas = [(0, -1), (0, 1)]
+
+        case "-":
+            deltas = [(-1, 0), (1, 0)]
+
+        case "L":
+            deltas = [(0, -1), (1, 0)]
+
+        case "J":
+            deltas = [(0, -1), (-1, 0)]
+
+        case "7":
+            deltas = [(0, 1), (-1, 0)]
+
+        case "F":
+            deltas = [(0, 1), (1, 0)]
+
+    # Dead end !
+    if len(deltas) == 0:
+        return None
+
+    first_exit = tuple(map(lambda x, y: x + y, cur_pos, deltas[0]))
+    second_exit = tuple(map(lambda x, y: x + y, cur_pos, deltas[1]))
+    if first_exit == from_pos:
+        return second_exit
+    if second_exit == from_pos:
+        return first_exit
+
+    # Invalid jump !
+    return None
+
+
+def find_loop(grid: dict[POS, str], start_pos: POS) -> list[POS]:
+
+    def pipe_explorer(seen: list[POS], cur_pos: POS | None, from_pos: POS) -> list[POS]:
+
+        if cur_pos in seen:
+            return seen
+
+        if cur_pos is None:
+            return []
+
+        seen.append(cur_pos)
+        return pipe_explorer(seen, find_next(grid, cur_pos, from_pos), cur_pos)
+
+    return max(pipe_explorer([start_pos], (start_pos[0], start_pos[1] + 1), start_pos),
+               pipe_explorer([start_pos], (start_pos[0] + 1, start_pos[1]), start_pos),
+               pipe_explorer([start_pos], (start_pos[0], start_pos[1] - 1), start_pos),
+               pipe_explorer([start_pos], (start_pos[0] - 1, start_pos[1]), start_pos),
+               key=lambda x: len(x))
+
+
+def main(input_file: str) -> int:
+    """
+    https://adventofcode.com/2023/day/7#part1
+    """
+    # Large input
+    sys.setrecursionlimit(25_000)
+    possible_nest = set()
+
+    grid, start_pos = parse_input(input_file)
+    loop = find_loop(grid, start_pos)
+
+    for y in range(max(pos[1] for pos in grid.keys())):
+        inside = False
+        prev_turn = ""
+
+        for x in range(max(pos[0] for pos in grid.keys())):
+            if (x, y) in loop:
+                if grid[x, y] in "7FSJL":
+                    if prev_turn == "":
+                        prev_turn = grid[x, y]
+                    else:
+                        if (prev_turn in "JSL" and grid[x, y] in "7F") or (prev_turn in "7F" and grid[x, y] in "SJL"):
+                            inside = not inside
+
+                        prev_turn = ""
+
+                if grid[x, y] == "|":
+                    inside = not inside
+
+            else:
+                if inside:
+                    possible_nest.add((x, y))
+
+    return len(possible_nest)
+
+
+if __name__ == '__main__':  # pragma: no cover
+    if len(sys.argv) != 2:
+        raise ValueError(f"Invalid parameter, usage : {sys.argv[0]} <input_file.txt>")
+    print(main(sys.argv[1]))
--- aoc2023/day10/test_day10.py
+++ aoc2023/day10/test_day10.py
@@ -1,5 +1,5 @@
 import day10.ex1.solver as solver_ex1
-# import day09.ex2.solver as solver_ex2
+import day10.ex2.solver as solver_ex2
 
 DAY = "day10"
 
@@ -8,11 +8,11 @@ EX1
 """
 
 
-def test_ex1_test_simple():
+def test_ex1_test_1():
     assert solver_ex1.main(f"{DAY}/ex1/test_1.txt") == 4
 
 
-def test_ex1_test_medium():
+def test_ex1_test_2():
     assert solver_ex1.main(f"{DAY}/ex1/test_2.txt") == 8
 
 
@@ -21,5 +21,21 @@ EX2
 """
 
 
-# def test_ex2():
-#     assert solver_ex2.main(f"{DAY}/ex2/test.txt") == 2
+def test_ex2_test_1():
+    assert solver_ex2.main(f"{DAY}/ex2/test_1.txt") == 1
+
+
+def test_ex2_test_2():
+    assert solver_ex2.main(f"{DAY}/ex2/test_2.txt") == 4
+
+
+def test_ex2_test_3():
+    assert solver_ex2.main(f"{DAY}/ex2/test_3.txt") == 8
+
+
+def test_ex2_test_4():
+    assert solver_ex2.main(f"{DAY}/ex2/test_4.txt") == 10
+
+
+def test_ex2_test_5():
+    assert solver_ex2.main(f"{DAY}/ex2/test_5.txt") == 15
