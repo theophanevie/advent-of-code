142d09d05cf8ae244b3ce428590f8dc87c064197
Théophane Vié <theophane.vie@petit-nuage.org>
2025-03-06 18:47:26 +0100

    feat(2020-d18): refacto using shunting yard

--- README.md
+++ README.md
@@ -6,10 +6,21 @@
 
 -- Eric Wastl, Advent of Code maker
 
-I do it with friends and colleagues, and it's always fun to discuss our solutions. Here are my attempts:
+I do it with friends and colleagues, and it's always fun to discuss our solutions. Here are my attempts.
+
+A non-exhaustive list of some used algorithms / math background :
+
+- [Minimum_cut](https://en.m.wikipedia.org/wiki/Minimum_cut) with [Karger's_algorithm](https://en.m.wikipedia.org/wiki/Karger%27s_algorithm)
+- [Shoelace_formula](https://en.wikipedia.org/wiki/Shoelace_formula)
+- [Pick's_theorem](https://en.wikipedia.org/wiki/Pick%27s_theorem)
+- [Reverse_Polish_notation](https://en.wikipedia.org/wiki/Reverse_Polish_notation)
+- [Shunting_yard_algorithm](https://en.wikipedia.org/wiki/Shunting_yard_algorithm)
+- [Quadratic_form](https://en.wikipedia.org/wiki/Quadratic_form)
+- [Taxicab_geometry](https://en.wikipedia.org/wiki/Taxicab_geometry)
 
 ## 2023 Edition -> 50 :star2: :tada:
 
 [![Coverage](https://theophanevie.github.io/advent-of-code/coverage.svg?raw=true)](https://github.com/theophanevie/aoc/actions/workflows/aoc2023.yaml)
 
 ![](static/2023.png)
+&
--- aoc2020/day18/solution.py
+++ aoc2020/day18/solution.py
@@ -1,36 +1,85 @@
 from sys import argv
+from collections import deque
 
 expressions = []
+
+# Thanks to https://en.wikipedia.org/wiki/Shunting_yard_algorithm
 
 with open(argv[1], "r") as f:
     for line in f.readlines():
         expressions.append(line.replace("(", "( ").replace(")", " )").split())
 
+def shunt(tokens: deque) -> deque:
+    operator = deque()
+    output = deque()
 
-def eval_expression(exp, sign):
-    buffer = []
+    while len(tokens) > 0:
+        token = tokens.popleft()
 
-    while len(exp) > 0:
-        token, exp = exp[0], exp[1:]
+        if token.isdigit():
+            output.append(token)
 
-        if token == "(":
-            exp = eval_expression(exp)
+        elif token in operators.keys():
+
+            while len(operator) > 0 and operator[-1] in operators.keys() and operators[operator[-1]] >= operators[token]:
+                output.append(operator.pop())
+
+            operator.append(token)
+
+        elif token == "(":
+            operator.append(token)
 
         elif token == ")":
-            return buffer + exp
+            assert len(operator) > 0
 
-        elif token.isdigit() or token in ["*", "+"]:
-            buffer.append(token)
+            while operator[-1] != "(":
+                output.append(operator.pop())
+
+            assert operator[-1] == "("
+            operator.pop()
 
         else:
-            assert False
+            assert False  # Sanity check
 
-        if len(buffer) == 3:
-            exp = [str(eval("".join(buffer)))] + exp
-            buffer = []
+    while len(operator) > 0:
+        assert operator[-1] != "("
+        output.append(operator.pop())
 
-    return buffer + exp
+    return output
+
+def eval_shunt(tokens: deque) -> int:
+    buffer = []
+    while len(tokens) > 0:
+        token = tokens.popleft()
+
+        if token.isdigit():
+            buffer.append(token)
+
+        elif token in operators.keys():
+            assert len(buffer) >= 2
+            buffer = buffer[:-2] + [eval(f"{buffer[-2]}{token}{buffer[-1]}")]
+
+        else:
+            assert False  # Sanity check
+
+    assert len(buffer) == 1
+    return buffer[0]
 
 
-part1 = sum(int(eval_expression(expression)[0]) for expression in expressions)
-print(part1)
+# All operators are left-associative
+operators = {
+    "+": 1,
+    "*": 1,
+}
+
+part1 = sum([eval_shunt(shunt(deque(exp))) for exp in expressions])
+print(f"Part 1: {part1}")
+
+# All operators are left-associative
+operators = {
+    "+": 1,
+    "*": 0,
+}
+
+part2 = sum([eval_shunt(shunt(deque(exp))) for exp in expressions])
+print(f"Part 2: {part2}")
